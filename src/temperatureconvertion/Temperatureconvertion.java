cc8b0d5d7172c39410620d697ec81913a83a8f17
Mohamed Sano <150654331+Sanomohamed@users.noreply.github.com>
2023-12-20 14:10:00 +0800

    Reviewing all my java code publish all my code on Git

---
+++ src/temperatureconvertion/Temperatureconvertion.java
@@ -0,0 +1,18 @@
+package temperatureconvertion;
+import java.util.*;
+public class Temperatureconvertion {
+     
+    public static void main(String[] args) {
+       System.out.println("Conversion");
+  
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please Enter the Temperature in degree");
+    double temp = sc.nextDouble();
+    
+    temperatureFahrenhei tp = new temperatureFahrenhei(temp);
+     
+    System.out.printf("\nThe value of the Temperature in Kelvin is %.2f\n", tp.Kelvin());
+    System.out.printf("The value of the Temperature in Fahrenheit is %.2f\n", tp.Fahrenhei());
+    
+    } 
+}
